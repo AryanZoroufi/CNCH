394699bd3e474bb39ed8693ce66d29ad38755241
AryanZoroufi <59450117+AryanZoroufi@users.noreply.github.com>
2020-09-01 10:46:05 +0430

    Delete DrawFixationCroos.m

--- Psychophysic_Code/DrawFixationCroos.m
+++
@@ -1,7 +0,0 @@
-function DrawFixationCroos(WindowPtr,crosslines,crosswidth,crosscolor,xCenter,yCenter)
-% ====================== YOUR CODE HERE ======================
-% hint: Draw fixation cross using DrawLines
-Screen('drawlines',WindowPtr,crosslines,crosswidth,crosscolor,[xCenter,yCenter]);
-Screen('Flip',WindowPtr);
-%==============================================================
-end
